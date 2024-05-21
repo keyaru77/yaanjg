c84a43b06a689524c44c565cc5aae4cc2c87ad83
keyaru77 <whyudacok@gmail.com>
2024-05-21 15:30:53 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,19 @@
+{
+  "name": "animasu-scraper",
+  "version": "1.0.0",
+  "description": "Animasu Scraper API",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "axios": "^0.27.2",
+    "cheerio": "^1.0.0-rc.12",
+    "express": "^4.18.1"
+  },
+  "engines": {
+    "node": ">=14.x"
+  },
+  "author": "",
+  "license": "ISC"
+}
