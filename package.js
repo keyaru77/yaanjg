87104b6c5786b42484a7f41087d43a71201c978d
keyaru77 <whyudacok@gmail.com>
2024-05-24 00:21:02 +0700

    Update package.js

--- package.js
+++ package.js
@@ -1,19 +1,26 @@
 {
-  "name": "animasu-scraper",
+  "name": "anime-scraper",
   "version": "1.0.0",
-  "description": "Animasu Scraper API",
+  "description": "A web scraper for anime",
   "main": "index.js",
   "scripts": {
-    "start": "node index.js"
+    "start": "node index.js",
+    "dev": "nodemon index.js"
   },
   "dependencies": {
     "axios": "^0.27.2",
-    "cheerio": "^1.0.0-rc.12",
-    "express": "^4.18.1"
+    "axios-cookiejar-support": "^3.0.0",
+    "cheerio": "^1.0.0-rc.10",
+    "cors": "^2.8.5",
+    "express": "^4.18.1",
+    "express-http-proxy": "^1.6.3",
+    "helmet": "^5.0.2",
+    "tough-cookie": "^4.0.0"
   },
-  "engines": {
-    "node": ">=14.x"
+  "devDependencies": {
+    "nodemon": "^2.0.16"
   },
+  "keywords": [],
   "author": "",
   "license": "ISC"
 }
